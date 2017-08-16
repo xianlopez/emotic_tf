488c2db098c0e7a65a4782ef76c46e96122089c2
xianlopez <lopezalvarez.xian@gmail.com>
2017-08-16 11:23:30 +0200

    whatever...

--- matlab_minimal_image/proba2.m
+++ matlab_minimal_image/proba2.m
@@ -3,17 +3,7 @@ close all
 
 addpath(genpath(pwd));
 
-class_names = {'tallbuilding', 'mountain', 'Opencountry', 'street', ...
-    'inside_city', 'highway', 'forest', 'coast'};
-
-% I = double(imread('images/art608.jpg')); truelabel = 6;
-% I = double(imread('images/art392.jpg')); truelabel = 1;
-% I = double(imread('images/art764.jpg')); truelabel = 4;
-% I = double(imread('images/bea14.jpg')); truelabel = 8;
-% I = double(imread('images/for157.jpg')); truelabel = 7;
-% I = double(imread('images/land18.jpg')); truelabel = 2;
-% I = double(imread('images/land964.jpg')); truelabel = 3;
-I = double(imread('images/urb307.jpg')); truelabel = 5;
+I = double(imread('/home/xian/EMOTIC/EmpathyDB_images/ade20k/images/labelme_axxlfpyqbzhgkgf.jpg'));
 
 I = imresize(I,256/min(size(I,1), size(I,2)));
 
@@ -31,8 +21,19 @@ param.minlength = 70; % example: 2; for edges (larger=> less edges)
 param.maxdist = 60;%50; % example: 10 in grey scale, 60 in rgb; for segments (larger => less segments)
 param.Ncorners = 10; % example: 10; number of corners
 
-
+% Remove previous bases:
+delete bases/*.png
 
 % generate bases --------------------------------------------------------
 bases = buildGradientBases(I, method, param);
-% showBases(bases);
+showBases(bases);
+
+for i = 1:bases.Nbases
+    imwrite(bases.B(:,:,:,i), ['bases/base',num2str(i),'.png']);
+end
+
+
+
+
+
+
